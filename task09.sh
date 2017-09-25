64ba65e630101af6ae91e525e55d0b363f8f4897
Matthew Maguire <matthewm@kainos.com>
2017-09-25 01:28:50 +0100

    Added solution to task09

---
+++ task09.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ ! -d ../git-game ]
+then
+echo "Bot directory not adjacent to git-game directory. Exiting"
+exit 1
+fi
+
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+cd ../git-game
+echo -e "=======\nLevel 9\n======="
+echo -e "${YELLOW}$ cat README.md${NC}"
+cat README.md
+echo -e "${YELLOW}$ git tag -d Henry${NC}"
+git tag -d Henry
+echo -e "${YELLOW}$ git checkout Henry${NC}"
+git checkout Henry
+
+if [ "$1" == "--finish" ]
+then
+cd ../Git-Hub-Bot
+bash task04 --finish
+else
+cat README.md
+cd ../Git-Hub-Bot
+fi
+
+exit 0
