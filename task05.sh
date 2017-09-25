ff508d9312336d1344054b1b7eaad31d74f89f4a
Matthew Maguire <matthewm@kainos.com>
2017-09-25 01:10:52 +0100

    Added solution to task05

---
+++ task05.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ ! -d ../git-game ]
+then
+echo "Bot directory not adjacent to git-game directory. Exiting"
+exit 1
+fi
+
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+cd ../git-game
+echo -e "=======\nLevel 5\n======="
+echo -e "${YELLOW}$ cat .gitignore${NC}"
+cat .gitignore
+echo -e "${YELLOW}$ git branch -a${NC}"
+git branch -a
+echo -e "${YELLOW}$ git checkout tree${NC}"
+git checkout tree
+
+if [ "$1" == "--finish" ]
+then
+cd ../Git-Hub-Bot
+bash task04 --finish
+else
+cat README.md
+cd ../Git-Hub-Bot
+fi
+
+exit 0
+
+
+cat .gitignore
+echo -e "${YELLOW}$ git checkout tree${NC}"
+git checkout tree
