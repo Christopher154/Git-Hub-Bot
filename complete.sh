5ec36daa4ce36e46cdedfcbe817c3ca42f339fde
Daniel Penney-Gallagher <penneygallagher@gmail.com>
2017-09-25 12:34:15 +0000

    Previous commit was a lie

--- complete.sh
+++ complete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ ! $# -eq 2 || ! $# -eq 3 ]]
+if [[ $# -ne 1 && $# -ne 2 ]]
 then
  echo "Usage: ./complete [current level] (--finish)"
  exit 1
@@ -26,28 +26,28 @@ then
  git checkout 6402738
  if [ "$2" == "--finish" ]
  then
-  flag = "finish"
+  flag="finish"
  else
   exit 0
  fi
 fi
 
-if [ "$1" == "2" || "$flag" == "finish" ]
+if [[ "$1" == "2" || "$flag" == "finish" ]]
 then
  echo -e "=======\nLevel 2\n======="
  echo -e "${YELLOW}$ git branch -a${NC}"
  git branch -a
  echo -e "${YELLOW}$ git checkout bug${NC}"
  git checkout bug
- if [ "$2" == "--finish"]
+ if [ "$2" == "--finish" ]
  then
-  flag = "finish"
+  flag="finish"
  else
   exit 0
  fi
 fi
 
-if [ "$1" == "3" || "$flag" == "finish" ]
+if [[ "$1" == "3" || "$flag" == "finish" ]]
 then
  echo -e "=======\nLevel 3\n======="
  echo -e "${YELLOW}$ git blame cool.cpp${NC}"
@@ -56,56 +56,56 @@ then
  git branch -a
  echo -e "${YELLOW}$ git checkout LinusTorvalds2014${NC}"
  git checkout LinusTorvalds2014
- if [ "$2" == "--finish"]
+ if [ "$2" == "--finish" ]
  then
-  flag = "finish"
+  flag="finish"
  else
   exit 0
  fi
 fi
 
-if [ "$1" == "4" || "$flag" == "finish" ]
+if [[ "$1" == "4" || "$flag" == "finish" ]]
 then
  echo -e "=======\nLevel 4\n======="
  echo -e "${YELLOW}$ ls -a${NC}"
  ls -a
- if [ "$2" == "--finish"]
+ if [ "$2" == "--finish" ]
  then
-  flag = "finish"
+  flag="finish"
  else
   exit 0
  fi
 fi
 
-if [ "$1" == "5" || "$flag" == "finish" ]
+if [[ "$1" == "5" || "$flag" == "finish" ]]
 then
  echo -e "=======\nLevel 5\n======="
  echo -e "${YELLOW}$ git branch -a${NC}"
  git branch -a
  echo -e "${YELLOW}$ git checkout tree${NC}"
  git checkout tree
- if [ "$2" == "--finish"]
+ if [ "$2" == "--finish" ]
  then
-  flag = "finish"
+  flag="finish"
  else
   exit 0
  fi
 fi
 
-if [ "$1" == "6" || "$flag" == "finish" ]
+if [[ "$1" == "6" || "$flag" == "finish" ]]
 then
  echo -e "=======\nLevel 6\n======="
  echo -e "${YELLOW}$ cat README.md${NC}"
  cat README.md
- if [ "$2" == "--finish"]
+ if [ "$2" == "--finish" ]
  then
-  flag = "finish"
+  flag="finish"
  else
   exit 0
  fi
 fi
 
-if [ "$1" == "7" || "$flag" == "finish" ]
+if [[ "$1" == "7" || "$flag" == "finish" ]]
 then
  echo -e "=======\nLevel 7\n======="
  echo -e "${YELLOW}$ ./outputclue.sh nextclue_input.cpp${NC}"
@@ -124,15 +124,15 @@ then
  ./outputclue.sh nextclue_input.cpp
  echo -e "${YELLOW}$ git checkout mouse${NC}"
  git checkout mouse
- if [ "$2" == "--finish"]
+ if [ "$2" == "--finish" ]
  then
-  flag = "finish"
+  flag="finish"
  else
   exit 0
  fi
 fi
 
-if [ "$1" == "8" || "$flag" == "finish" ]
+if [[ "$1" == "8" || "$flag" == "finish" ]]
 then
  echo -e "=======\nLevel 8\n======="
  echo -e "${YELLOW}$ git diff origin/bug --remember${NC}"
@@ -141,30 +141,30 @@ then
  git branch -a
  echo -e "${YELLOW}$ git checkout Henry${NC}"
  git checkout Henry
- if [ "$2" == "--finish"]
+ if [ "$2" == "--finish" ]
  then
-  flag = "finish"
+  flag="finish"
  else
   exit 0
  fi
 fi
 
-if [ "$1" == "9" || "$flag" == "finish" ]
+if [[ "$1" == "9" || "$flag" == "finish" ]]
 then
  echo -e "=======\nLevel 9\n======="
  echo -e "${YELLOW}$ git tag -d Henry${NC}"
  git tag -d Henry
  echo -e "${YELLOW}$ git checkout Henry${NC}"
  git checkout Henry
- if [ "$2" == "--finish"]
+ if [ "$2" == "--finish" ]
  then
-  flag = "finish"
+  flag="finish"
  else
   exit 0
  fi
 fi
 
-if [ "$1" == "10" || "$flag" == "finish" ]
+if [[ "$1" == "10" || "$flag" == "finish" ]]
 then
  echo -e "=======\nLevel 10\n======="
  echo -e "${YELLOW}$ git checkout master${NC}"
