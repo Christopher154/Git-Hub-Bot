f3fcc955b88f05d64326fe4b32205c30a7eafc25
Peter Gordon <p.gordon@kainos.com>
2017-09-25 13:27:03 +0100

    Fixed reset script to allow no existing git-game directory

--- resetGame.sh
+++ resetGame.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-if [ ! -d ../git-game ]
-then
-echo "Bot directory not adjacent to git-game directory. Exiting"
-exit 1
-fi
-
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-echo -e "${YELLOW}Resetting git-game...${NC}"
-echo -e "${YELLOW}Removing old files...${NC}"
-rm -rf ../git-game
 cd ..
+echo -e "${YELLOW}Resetting git-game...${NC}"
+
+if [ -d git-game ]
+then
+echo -e "${YELLOW}Removing old files...${NC}"
+rm -rf git-game
+fi
+
 echo -e "${YELLOW}Cloning repository...${NC}"
 git clone https://github.com/hgarc014/git-game.git
-cd Git-Hub-Bot
 echo -e "${YELLOW}Success!${NC}"
-
-exit 0
