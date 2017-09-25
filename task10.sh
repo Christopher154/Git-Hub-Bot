bb0d95d00bd0d36205b2b500ed6ec016b3ef7222
Matthew Maguire <matthewm@kainos.com>
2017-09-25 01:31:33 +0100

    Added solution to task10

---
+++ task10.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ ! -d ../git-game ]
+then
+echo "Bot directory not adjacent to git-game directory. Exiting"
+exit 1
+fi
+
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+cd ../git-game
+echo -e "=======\nLevel 10\n======="
+echo -e "${YELLOW}$ cat README.md${NC}"
+cat README.md
+echo -e "${YELLOW}$ git checkout master${NC}"
+git checkout master
+echo -e "${YELLOW}$ git remote add updater git@github.com:drami025/git-game.git${NC}"
+git remote add updater git@github.com:drami025/git-game.git
+echo -e "${YELLOW}$ git pull updater master${NC}"
+git pull updater master
+echo -e "${YELLOW}$ cat README.md${NC}"
+cat README.md
+
+if [ "$1" == "--finish" ]
+then
+cd ../Git-Hub-Bot
+bash task04 --finish
+else
+cat README.md
+cd ../Git-Hub-Bot
+fi
+
+exit 0
