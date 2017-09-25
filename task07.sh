f542f9180cd7a0f438717b8eba86c90d82c217a2
Matthew Maguire <matthewm@kainos.com>
2017-09-25 01:25:07 +0100

    Added partial solution to task07

---
+++ task07.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ ! -d ../git-game ]
+then
+echo "Bot directory not adjacent to git-game directory. Exiting"
+exit 1
+fi
+
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+cd ../git-game
+echo -e "=======\nLevel 7\n======="
+echo -e "${YELLOW}$ ./outputclue.sh nextclue_input.cpp${NC}"
+./outputclue.sh nextclue_input.cpp
+echo -e "${YELLOW}$ git merge code4life${NC}"
+git merge code4life
+echo -e "${YELLOW}$ git status${NC}"
+git status
+read -p "Fix merge conflicts and press enter"
+echo -e "${YELLOW}$ git commit -am"Fixed merge conflicts"${NC}"
+git commit -am"Fixed merge conflicts"
+echo -e "${YELLOW}$ git status${NC}"
+git status
+echo -e "${YELLOW}$ ./outputclue.sh nextclue_input.cpp${NC}"
+./outputclue.sh nextclue_input.cpp
+echo -e "${YELLOW}$ git checkout mouse${NC}"
+git checkout mouse
+
+if [ "$1" == "--finish" ]
+then
+cd ../Git-Hub-Bot
+bash task04 --finish
+else
+cat README.md
+cd ../Git-Hub-Bot
+fi
+
+exit 0
